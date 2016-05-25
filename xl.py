19d945c3547d44fc764b7f3a2a8d27f5aae1046c
Evgeny Pogrebnyak <e.pogrebnyak@gmail.com>
2016-05-26 02:35:19 +0300

    Add files via upload

--- xl.py
+++ xl.py
@@ -1,14 +1,13 @@
-#Implementation:
-# - requirement - Windows machine with Microsoft Excel
-# - Anaconda package suggested for libraries
-# - Python 3.5 
-
-#For improvement(not todo now):
-# - make default sheet number/name a class variable
-# - test 'formulas' with py.test
-
 """
-DESCRIPTION
+Requirements
+------------
+ - Windows machine with Microsoft Excel
+ - Anaconda package suggested for libraries
+ - Python 3.5 
+
+Description
+-----------
+
 User story: 
   - the user wants to automate filling formulas on Excel sheet 
   - Excel sheet has simple 'roll forward' forecast - a spreadsheet model with some 
@@ -28,20 +27,21 @@ Some rules:
   - we need explicit specification of year when the forecast starts -  by 'is_forecast' vector 
       
 Simplifications/requirements:
-  critical, but not checked: 
-  - time series in rows only, horizontal orientation 
-  - dataset starts at A1 cell
-  checked:
-  - must have 'is_forecast' vector in dataset
-  not critical:
-  - datablock is next to variable labels
-  - time labels are years, not checked for continuity
+  - critical, but not checked: 
+     - time series in rows only, horizontal orientation 
+     - dataset starts at A1 cell
+  - checked:
+     - must have 'is_forecast' vector in dataset
+  - not critical:
+     - datablock is next to variable labels
+     - time labels are years, not checked for continuity
 
-Main functionality: fill cells in Excel sheet with formulas (e.g. '=C3*D4') based on 
+Main functionality: 
+- fill cells in Excel sheet with formulas (e.g. '=C3*D4') based on 
                     list of variable names and equations.
-                    
-                    Formulas go only to forecast periods columns (where is_forecast == 1) 
+- formulas go only to forecast periods columns (where is_forecast == 1) 
 
+```
 Input Excel sheet:
 -------------------------------
             A     B     C     D
@@ -61,6 +61,7 @@ Output Excel sheet:
 4         rog              1.05
 6 y = y[t-1] * rog
 --------------------------------
+```
 
 Comment:
 - 'year' is not used in calculations 
@@ -68,21 +69,112 @@ Comment:
 - 'y' is data variable
 - 'rog' is control parameter
 - 'y = y[t-1] * rog' is formula (equation)
+
+Todo later
+----------
+ - code review
+ - make default sheet number/name a class variable + other changes related to sheet handling
+ - test 'formulas' with py.test
+
 """
 
 import pandas as pd
 import numpy as np
 from xlwings import Workbook, Range, Sheet
 import os
+from collections import OrderedDict
+import xlrd
+import re
 
-from formulas import to_equations_dict, parse_equation_to_xl_formula
+class Segment():
+    
+    def __init__(self, text, var_to_rows):
+       
+        varname, b = re.search(r'(\w+)\[(\d+)\]', text).groups()
+        self.col = int(b)
+        if varname in var_to_rows.keys():
+           self.row = var_to_rows[varname]
+        else:
+           raise KeyError("Variable without row: " + varname)
+            
+    
+    def get_xl_ref(self):
+        return xlrd.colname(self.col) + str(self.row)
+
+class Formula():
+
+    def __init__(self, equation_string, var_to_rows, period):
+        
+        # strip all whitespace        
+        equation_string = re.sub(r'\s+', '', equation_string)
+        equation_string = self.expand_shorthand(equation_string, var_to_rows)
+        equation_string = self.evaluate_time_indices(equation_string, period)
+
+        segments = re.findall(r'(\w+\[\d+\])', equation_string)    
+        for seg in segments:
+             xl_ref = Segment(seg, var_to_rows).get_xl_ref()
+             # match beginning of word
+             equation_string = re.sub(r'\b' + re.escape(seg), xl_ref, equation_string)
+             
+        self.text = '=' + equation_string
+
+    @staticmethod    
+    def expand_shorthand(text, var_to_rows):
+        for var in [v for v in var_to_rows.keys() if v]:
+           text = re.sub(var + r'(?!\s*[\dA-Za-z_^\[])', var + '[t]', text)
+        return text  
+
+    @staticmethod    
+    def evaluate_time_indices(text, period):
+       
+        T_ONLY_REGEX = r'\[([t+\-\d]+)\]'
+        
+        for time_index_expression in re.findall(T_ONLY_REGEX, text):
+            try:
+                t = period
+                period_offset = eval(time_index_expression)
+            except:
+                raise ValueError('Time expression %s[%s] invalid' % (var, period))
+            
+            text = text.replace('[' + time_index_expression + ']', 
+                                '[' + str(period_offset)    + ']')
+        return text         
+
+class Equations():
+    
+    def __init__(self, equation_strings):
+        
+        eq_dict = OrderedDict()
+        # disregard comments and strings without '='
+        equation_strings = [eq for eq in equation_strings 
+                            if "=" in eq and not eq.strip().startswith("#")]
+        for eq in equation_strings:
+            key, formula = self.parse_equation_string(eq)
+            if key in eq_dict.keys():
+                self.error_duplicate_equation(key, eq_dict[key], formula)                    
+            else:
+                eq_dict[key] = formula.strip()
+        self.dict = eq_dict  
+        
+    @staticmethod    
+    def parse_equation_string(string):
+        left_hand_side_expression, formula = string.split('=')
+        varname = left_hand_side_expression.replace(" ","").replace("[t]", "")    
+        return varname, formula                     
+
+    @staticmethod    
+    def error_duplicate_equation(key, eq1, eq2):
+        raise ValueError("Two equations for the same variable. " + 
+                         "\nVariable: " + key +          
+                         "\nExisting equation: " + eq1 +
+                         "\nAlternative equation: " + eq2)
 
 class Model():
     """    
     Fill dataset with formulas containing A1 cell references based on 
     equations and variable locations on Excel sheet: 
     
-        dataset_with_formulas = Model(equations, dataset, var_to_rows).xl_dataset
+    dataset_with_formulas = Model(equations, dataset, var_to_rows).xl_dataset
     
     """
     
@@ -104,18 +196,18 @@ class Model():
         
         # warning: using dataset.copy() to prevent the global arguement from being modified inside the class
         self.dataset = dataset.copy()
-        self.equations = to_equations_dict(equations) 
+        self.equations = Equations(equations).dict 
         self.var_to_rows = var_to_rows
-        self.varname_col_n = col_to_num(varname_column) # column 2, not used
+        self.varname_col_n = self.col_to_num(varname_column) # column 2, not used
         
         self._validate_is_forecast()
         self._validate_math_model()
         self._validate_positioning()
         
         # main functionalty 
-        self.xl_dataset = self.make_xl_dataset()
+        self.xl_dataset = self.get_xl_dataset()
         
-    def make_xl_dataset(self):
+    def get_xl_dataset(self):
         
         xl_dataset = self.dataset 
         
@@ -124,29 +216,27 @@ class Model():
         
         # for each ariable name on left hand side of equations...          
         for varname in self.equations.keys():
-            # know thire formula
-            formula = self.equations[varname]
+            # know its formula
+            equation = self.equations[varname]
             # .. go over forecast time periods... 
             for i in forecast_index_positions:
                # later: must change 'period' to 'current_column_n' and review 'period' in 'parse_equation_to_xl_formula'
                period = i + 1
                # .... and assign formulas in xl_dataset
                xl_dataset.loc[xl_dataset.index[i], varname] = \
-                        parse_equation_to_xl_formula(formula, self.var_to_rows, period)
+                        Formula(equation, self.var_to_rows, period).text
                         
         return xl_dataset
                        
-    def get_xl_dataset(self):         
-        return self.xl_dataset       
-        
-def col_to_num(col_str):
-    """ Convert base26 column string to number. """
-    expn = 0
-    col_num = 0
-    for char in reversed(col_str):
-        col_num += (ord(char) - ord('A') + 1) * (26 ** expn)
-        expn += 1
-    return col_num
+    @staticmethod
+    def col_to_num(col_str):
+        """ Convert base26 column string to number. """
+        expn = 0
+        col_num = 0
+        for char in reversed(col_str):
+            col_num += (ord(char) - ord('A') + 1) * (26 ** expn)
+            expn += 1
+        return col_num
     
 class SingleSheet():
     """
@@ -211,23 +301,23 @@ class SingleSheet():
         # later: must check sheet_n exists
         Sheet(sheet_n).activate()
         # later: move 'A1' to CORNER_CELL = 'A1'
-        Range(sheet_n, 'A1').value = to_matrix(self.image_df)
+        Range(sheet_n, 'A1').value = self.to_matrix(self.image_df)
         wb.save()
         
-        
-def to_matrix(df): 
-    df = df.fillna("")
-    line0 = [""] +  df.columns.tolist()
-    lines = [line0]
-    for ix in df.index:
-       def to_int(x):
-           if type(x) == float and round(x) == x:
-               return int(x)
-           else:
-               return x
-       row = [to_int(x) for x in df.loc[ix,].tolist()] 
-       lines.append([ix] + row) 
-    return [[str(x) for x in line] for line in lines]    
+    @staticmethod    
+    def to_matrix(df): 
+        df = df.fillna("")
+        line0 = [""] +  df.columns.tolist()
+        lines = [line0]
+        for ix in df.index:
+           def to_int(x):
+               if type(x) == float and round(x) == x:
+                   return int(x)
+               else:
+                   return x
+           row = [to_int(x) for x in df.loc[ix,].tolist()] 
+           lines.append([ix] + row) 
+        return [[str(x) for x in line] for line in lines]    
 
         
 if __name__ == "__main__":    
@@ -282,7 +372,20 @@ if __name__ == "__main__":
     assert is_equal(ref_df, ms.model.get_xl_dataset()) 
     
     #later: not sure it is 1, must switch to     
-    assert '=B10' == parse_equation_to_xl_formula('credit', {'credit':10}, 1)
+    assert '=B10' == Formula('credit', {'credit':10}, 1).text
     #assert '=B2+A3*100' == parse_equation_to_xl_formula('GDP[t] + GDP_IQ[t-1] * 100'
     #                       , {'GDP': 1, 'GDP_IQ': 2, 'GDP_IP': 3}, 1)
+    
+    fs = _Segment("GDP[1]",{'GDP':100})    
+    assert fs.col == 1
+    assert fs.row == 100
+    assert fs.get_xl_ref() == 'B100'
+    
+    assert Formula("GDP[1]  \t   ",          {'GDP':100}, 1).text == "=B100"
+    assert Formula("0.5*(GDP[0]   +GDP[1])", {'GDP':100}, 1).text == '=0.5*(A100+B100)'
+    
+    f = Formula("GDP[t]", {'GDP':100}, 1)
+    assert f.text == "=B100"
+    assert f.evaluate_time_indices('GDP[t]+GDP[t-1]+0.5*GDP_IP[t]', 1) == 'GDP[1]+GDP[0]+0.5*GDP_IP[1]'
+
     
